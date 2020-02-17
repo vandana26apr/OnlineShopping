3a971d2e4d1478961231c0e4f17cf2d54b4a4cd3
vandana26apr <59745028+vandana26apr@users.noreply.github.com>
2020-02-17 16:25:51 +0530

    test2

--- react-shopping-cart/test/test2.js
+++ react-shopping-cart/test/test2.js
@@ -1,5 +1,6 @@
 var request =require("request");
 const https =require("http");
+var expect =require("expect");
 
 
 describe('Cart details', function()
@@ -17,7 +18,13 @@ it('status of cart api', function(done)
        {
           console.log(body);
           var resbody=JSON.parse(body);
-
+          var count_curr=0;
+        for (var each_ele in resbody.products){
+              //console.log(json_body.products[each_ele].availableSizes[each_av]);
+              if(resbody.products[each_ele].currencyId == "USD"){
+                 count_curr=count_curr+1;
+              }
+        }
        }
       else 
       console.log("this is not a valid url");
@@ -26,4 +33,5 @@ it('status of cart api', function(done)
 
 
    })
+   
 })   
